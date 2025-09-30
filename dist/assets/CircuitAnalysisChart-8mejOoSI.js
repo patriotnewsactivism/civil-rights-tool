30ab682134904564aee26bee33475db2ad46ee17
Fix Developer <user@example.com>
2025-09-30 23:52:49 +0000

    🔧 Fix: Consolidate tabs and add missing federalCircuits data structure
    - Consolidated 7 tabs into 5 cleaner tabs: Legal Analysis, Resources & Laws, Toolkits, Case Explorer, Legal Toolkit Pro
    - Combined Marijuana Laws and Recording Laws into Resources & Laws tab
    - Combined Activist and Journalist toolkits into unified Toolkits tab
    - Added missing federalCircuits data structure to fix application crashes
    - Created ResourcesAndLaws and ToolkitsHub components for better organization
    - Removed database connection banner for cleaner UI
    - Enhanced ID Rights Card with larger dimensions (500x350px) for better readability

--- dist/assets/CircuitAnalysisChart-8mejOoSI.js
+++ dist/assets/CircuitAnalysisChart-8mejOoSI.js
@@ -1,4 +1,4 @@
-var Mo=Object.defineProperty;var ko=(i,t,e)=>t in i?Mo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var M=(i,t,e)=>ko(i,typeof t!="symbol"?t+"":t,e);import{r as li,j as J}from"./index-89abTcIW.js";/*!
+var Mo=Object.defineProperty;var ko=(i,t,e)=>t in i?Mo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var M=(i,t,e)=>ko(i,typeof t!="symbol"?t+"":t,e);import{r as li,j as J}from"./index-BlR2PqGq.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
